18005865c85d54f6c519573535b58a4231eb7a25
Darshan <darsh19102002@gmail.com>
2026-01-27 23:14:24 +0530

    code fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-     name: {
-        type: String,
-        required: true,
-     },
-     email: {
-        type: String,
-        required: true,
-        unique: true,
-     },
-     image: {
-        type: String,
-        default: '',
-     }
-})
+  // Use Clerk user ID as the primary key so it's a string, not an ObjectId
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
